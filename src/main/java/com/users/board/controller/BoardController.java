799f546bd3177e85e8c8fb9e62633e66da76dc2f
MSK <mmmmk0219@gmail.com>
2023-10-20 20:08:27 +0900

    SpringBoot Board Practice First Commit

---
+++ src/main/java/com/users/board/controller/BoardController.java
@@ -0,0 +1,60 @@
+package com.users.board.controller;
+
+import com.users.board.dto.BoardDTO;
+import com.users.board.service.BoardService;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/board")
+// 중복되는 주소일 경우, @RequestMapping을 사용해 줄일 수 있다.
+public class BoardController {
+
+    private final BoardService boardService;
+
+    // 글작성 페이지 이동
+    @GetMapping("/save") // "/board/save"
+    public String saveForm() {
+        return "save";
+    }
+
+    // 글 작성 기능
+    @PostMapping("/save")
+    public String save(@ModelAttribute BoardDTO boardDTO) {
+        System.out.println("boardDTO = " + boardDTO);
+        boardService.save(boardDTO);
+        return "index";
+    }
+
+    // 전체 글 목록 조회
+    @GetMapping("/")
+    public String findAll(Model model) { // DB에서 데이터를 가지고 올때는 Model 객체를 사용한다.
+        // DB에서 전체 게시글 데이터를 가져와서 list.html에 출력한다.
+
+        List<BoardDTO> boardDTOList = boardService.findAll();
+        // 여러가지를 담아오기 위해선 List를 사용한다.
+
+        model.addAttribute("boardList", boardDTOList);
+        return "list";
+    }
+
+    // 특정 게시글 상세 조회
+    @GetMapping("/{id}")
+    public String findById(@PathVariable Long id, Model model) {
+        // @PathVariable 경로상에 존재하는 값을 받기 위해 사용.
+        // 상세 조회시 고려해야할 사항
+        // 1. 해당 게시글의 조회수를 1 올리고
+        // 게시글 데이터를 가져와서 detail.html에 출력하기
+
+        boardService.updateHits(id);
+        BoardDTO boardDTO = boardService.findById(id);
+        model.addAttribute("board", boardDTO);
+        return "detail";
+    }
+}
---
+++ src/main/java/com/users/board/dto/BoardDTO.java
@@ -0,0 +1,37 @@
+package com.users.board.dto;
+
+import com.users.board.entity.BoardEntity;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor // 기본 생성자
+@AllArgsConstructor // 모든 필드를 매개변수로 하는 생성자
+@ToString // 필드값을 확인할 떄 사용.
+// DTO = Data Transfer Object / VO, Bean 은 같다. Entity는 조금 다름.
+public class BoardDTO {
+    private Long id;
+    private String boardWriter;
+    private String boardPass;
+    private String boardTitle;
+    private String boardContents;
+    private int boardHits;
+    private LocalDateTime boardCreatedTime;
+    private LocalDateTime boardUpdatedTime;
+
+    // BoardEntity 타입의 객체들을 BoardDTO 타입으로 변경하는 메서드
+    public static BoardDTO toBoardDTO(BoardEntity boardEntity) {
+    BoardDTO boardDTO = new BoardDTO();
+    boardDTO.setId(boardEntity.getId());
+    boardDTO.setBoardWriter(boardEntity.getBoardWriter());
+    boardDTO.setBoardPass(boardEntity.getBoardPass());
+    boardDTO.setBoardTitle(boardEntity.getBoardTitle());
+    boardDTO.setBoardContents(boardEntity.getBoardContents());
+    boardDTO.setBoardHits(boardEntity.getBoardHits());
+    boardDTO.setBoardCreatedTime(boardEntity.getBoardCreatedTime());
+    boardDTO.setBoardUpdatedTime(boardEntity.getUpdateTime());
+    return boardDTO;
+    }
+}
