2f9e9fe3997ca7436113d2b9e0260950e66792de
MSK <mmmmk0219@gmail.com>
2023-10-24 18:38:48 +0900

    SpringBoot Board Practice 23.10.24 - Commit

--- src/main/java/com/users/board/dto/BoardDTO.java
+++ src/main/java/com/users/board/dto/BoardDTO.java
@@ -1,9 +1,13 @@
 package com.users.board.dto;
 
 import com.users.board.entity.BoardEntity;
+import com.users.board.entity.BoardFileEntity;
 import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -21,6 +25,13 @@ public class BoardDTO {
     private LocalDateTime boardCreatedTime;
     private LocalDateTime boardUpdatedTime;
 
+    // 파일 관련
+    // save.html -> Controller 파일을 담는 용도이다.
+    private List<MultipartFile> boardFile;
+    private List<String> originalFileName; // 원본 파일 이름
+    private List<String> storedFileName; // 서버 저장용 파일 이름.
+    private int fileAttached; // 파일 첨부 여부 확인 (첨부 1, 미첨부 0)
+
     public BoardDTO(Long id, String boardWriter, String boardTitle, int boardHits, LocalDateTime boardCreatedTime) {
         this.id = id;
         this.boardWriter = boardWriter;
@@ -31,15 +42,36 @@ public class BoardDTO {
 
     // BoardEntity 타입의 객체들을 BoardDTO 타입으로 변경하는 메서드
     public static BoardDTO toBoardDTO(BoardEntity boardEntity) {
-    BoardDTO boardDTO = new BoardDTO();
-    boardDTO.setId(boardEntity.getId());
-    boardDTO.setBoardWriter(boardEntity.getBoardWriter());
-    boardDTO.setBoardPass(boardEntity.getBoardPass());
-    boardDTO.setBoardTitle(boardEntity.getBoardTitle());
-    boardDTO.setBoardContents(boardEntity.getBoardContents());
-    boardDTO.setBoardHits(boardEntity.getBoardHits());
-    boardDTO.setBoardCreatedTime(boardEntity.getBoardCreatedTime());
-    boardDTO.setBoardUpdatedTime(boardEntity.getUpdateTime());
-    return boardDTO;
+        BoardDTO boardDTO = new BoardDTO();
+        boardDTO.setId(boardEntity.getId());
+        boardDTO.setBoardWriter(boardEntity.getBoardWriter());
+        boardDTO.setBoardPass(boardEntity.getBoardPass());
+        boardDTO.setBoardTitle(boardEntity.getBoardTitle());
+        boardDTO.setBoardContents(boardEntity.getBoardContents());
+        boardDTO.setBoardHits(boardEntity.getBoardHits());
+        boardDTO.setBoardCreatedTime(boardEntity.getBoardCreatedTime());
+        boardDTO.setBoardUpdatedTime(boardEntity.getUpdateTime());
+        if (boardEntity.getFileAttached() == 0) {
+            boardDTO.setFileAttached(boardEntity.getFileAttached()); // 0
+        } else if (!boardEntity.getBoardFileEntityList().isEmpty()) {
+            List<String> origianlFileNameList = new ArrayList<>();
+            List<String> storedFileNameList = new ArrayList<>();
+            boardDTO.setFileAttached(boardEntity.getFileAttached()); // 1
+            // 파일 이름을 가져가야한다.
+            // originalFIleName, storedFilename은 board_file_table에 저장되어 있다.
+            // board_file_table(BoardFIleEntity)
+            // select * from board_table b, board_file_table bf where b.id = bf.board_id
+            // and where b.id = ?
+            for (BoardFileEntity boardFileEntity : boardEntity.getBoardFileEntityList()) {
+                // boardDTO.setOriginalFileName(boardEntity.getBoardFileEntityList().get(0).getOriginalFileName());
+                // boardDTO.setStoredFileName(boardEntity.getBoardFileEntityList().get(0).getStoredFileName());
+
+                origianlFileNameList.add(boardFileEntity.getOriginalFileName());
+                storedFileNameList.add(boardFileEntity.getStoredFileName());
+            }
+            boardDTO.setOriginalFileName(origianlFileNameList);
+            boardDTO.setStoredFileName(storedFileNameList);
+        }
+        return boardDTO;
     }
 }
